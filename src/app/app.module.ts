159684fbbd8526f7ddc8c7facebe769477b7bbd0
Your Name <you@example.com>
2023-05-16 01:38:39 -0400

    get request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,6 +25,7 @@ import { MatInputModule } from '@angular/material/input';
     MatCheckboxModule,
     MatButtonModule,
     MatInputModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/services/quote.service.ts
+++ src/app/services/quote.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuoteService {
+  public getApiValue: any;
+  public postApiValue: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getQuote() {
+    this.http.get('https://api.quotable.io/random').subscribe((response) => {
+      this.getApiValue = response;
+      return this.getApiValue;
+    }
+  }
 }
