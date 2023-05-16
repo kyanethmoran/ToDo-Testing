82f92eba110e2d506d8f3c58d5a2915a2de94e21
Your Name <you@example.com>
2023-05-15 23:01:40 -0400

    header & item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+// import { Item } from './interfaces/item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ToDo-Testing';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { descriptiion: 'Get groceries', completed: false },
+    { descriptiion: 'Call plumber', completed: false },
+    { descriptiion: 'Buy running shoes', completed: false },
+    { descriptiion: 'Buy flowers', completed: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(
+      (item) => item.completed === (this.filter === 'done')
+    );
+  }
+
+  //add items to the list
+  addItem(descriptiion: string) {
+    this.allItems.push({
+      descriptiion,
+      completed: false,
+    });
+  }
+
+  //remove items from the list
+  remove(item: any) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemComponent } from './components/item/item.component';
+import { QuoteComponent } from './components/quote/quote.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, ItemComponent, QuoteComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
