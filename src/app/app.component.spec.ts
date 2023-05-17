3cb969b1c7ffa27215670c4a3a3bb20106c96df8
Your Name <you@example.com>
2023-05-16 20:35:57 -0400

    app component test cases written

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -31,4 +31,71 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ToDo-Testing');
   });
+
+  //test if we get the items
+  it('should get the items', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.items).toBeTruthy();
+  });
+
+  //test if we can add an item
+  it('should add an item', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const item = { description: 'test', completed: false };
+    app.addItem(item.description);
+    expect(app.items).toContain(item);
+  });
+
+  //test if Add button calls addItem
+  it('should call addItem with button click', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    spyOn(app, 'addItem');
+    const compiled = fixture.nativeElement;
+    const button = compiled.querySelector('button');
+    button.click();
+    expect(app.addItem).toHaveBeenCalled();
+  });
+
+  //test if 'Enter' key calls addItem
+  it('should call addItem with Enter key press', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    spyOn(app, 'addItem');
+    const compiled = fixture.nativeElement;
+    const input = compiled.querySelector('input');
+    const event = new KeyboardEvent('keyup', {
+      key: 'Enter',
+    });
+    input.dispatchEvent(event);
+    expect(app.addItem).toHaveBeenCalled();
+  });
+
+  //test if we can remove an item
+  it('should remove an item', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const item = { description: 'test', completed: false };
+    app.addItem(item.description);
+    app.remove(item);
+    expect(app.items).not.toContain(item);
+  });
+
+  //test if we render app-item
+  it('should render app-item', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-item')).toBeTruthy();
+  });
+
+  //test if we render app-quote
+  it('should render app-quote', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-quote')).toBeTruthy();
+  });
 });
