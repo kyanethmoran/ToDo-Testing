fd867c94aa2d965b6fe3fa52f6f40717b0efe295
Your Name <you@example.com>
2023-05-16 21:10:57 -0400

    test for getQuote() http get call

--- src/app/services/quote.service.spec.ts
+++ src/app/services/quote.service.spec.ts
@@ -1,22 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { QuoteService } from './quote.service';
+import { inject } from '@angular/core';
 
 describe('QuoteService', () => {
   let service: QuoteService;
+  let httpMock: HttpTestingController;
+  let injector: TestBed;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [QuoteService],
     });
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
   });
 
   it('should be created', () => {
     const service: QuoteService = TestBed.get(QuoteService);
     expect(service).toBeTruthy();
+  });
+
+  // test for getQuote()
+  it('should return a random quote', () => {
+    const service: QuoteService = TestBed.get(QuoteService);
+
+    service.getQuote().subscribe((quote) => {
+      expect(quote).toBeDefined();
+    });
+
+    const req = httpMock.expectOne('https://api.quotable.io/random');
+    expect(req.request.method).toBe('GET');
   });
 });
