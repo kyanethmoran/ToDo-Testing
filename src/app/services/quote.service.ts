173cde78369b2097c2753fb22e40a538a7b5cbdb
Your Name <you@example.com>
2023-05-16 21:10:16 -0400

    cleanup

--- src/app/services/quote.service.ts
+++ src/app/services/quote.service.ts
@@ -5,8 +5,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class QuoteService {
-  public quote: any;
-
   constructor(private http: HttpClient) {}
 
   public getQuote() {
